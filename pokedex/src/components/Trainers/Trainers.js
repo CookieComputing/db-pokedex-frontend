4cb0ec4a3c82308b96bef992873ce77186aa3906
Kevin Hui <hui.k@northeastern.edu>
2021-12-12 09:57:46 -0500

    add trainer pagination

--- pokedex/src/components/Trainers/Trainers.js
+++ pokedex/src/components/Trainers/Trainers.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createTrainer, findAllTrainers, updateTrainer, deleteTrainer } from '../../api/TrainerAPI';
-import Tab from 'react-bootstrap/Tab';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col'
 import ListGroup from 'react-bootstrap/ListGroup';
@@ -10,8 +9,7 @@ import Button from 'react-bootstrap/Button';
 import AddTrainersModal from "./AddTrainersModal";
 import EditTrainersModal from "./EditTrainersModal";
 import DelTrainersModal from "./DelTrainersModal";
-
-const { useState, useEffect } = React;
+import Pages from '../Page';
 
 export default function Trainers(props) {
     return <div>
@@ -23,74 +21,85 @@ export default function Trainers(props) {
     </div>
 }
 
-function PokemonTrainersList(props) {
-    const [pokemonTrainer, setTrainerInfo] = useState([])
-    const [pokemonTrainerIndex, setPokemonTrainerIndex] = useState(-1)
-    const [addModalVisible, setAddModalVisible] = useState(false);
-    const [editModalVisible, setEditModalVisible] = useState(false);
-    const [delModalVisible, setDelModalVisible] = useState(false);
+class PokemonTrainersList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokemonTrainer: [],
+            pokemonTrainerIndex: 0,
+            addModalVisible: false,
+            editModalVisible: false,
+            delModalVisible: false
+        }
+    }
 
-    useEffect(() => {
-        findAllTrainers().then(pokeTrainer => { setTrainerInfo(pokeTrainer) })
-    }, [])
-
-    const handleSave = async (payload) => {
-        await createTrainer(payload);
+    async handleSave(payload) {
+        createTrainer(payload);
         window.location.reload();
     }
 
-    const handleEdit = async (payload) => {
-        await updateTrainer(payload);
+    async handleEdit(payload) {
+        updateTrainer(payload);
         window.location.reload();
     }
 
-    const handleDel = async (payload) => {
-        await deleteTrainer(payload);
+    async handleDel(payload) {
+        deleteTrainer(payload);
         window.location.reload();
     }
 
-    return <div>
-        <h1>Trainers</h1>
-        <ListGroup as='ol' numbered>
-            {
-                pokemonTrainer.map((pokeTrainer, index) =>
-                    <ListGroup.Item as="li"
-                        className="d-flex justify-content-between align-items-start"
-                        key={pokeTrainer.pk}>
-                        <div className="ms-2 me-auto">
-                            <div className="fw-bold">{pokeTrainer.fields.first_name} {pokeTrainer.fields.last_name}</div>
-                        </div>
-                        <Badge variant="dark" className="btn-primary me-2 align-self-center" pill>
-                            {pokeTrainer.pk}
-                        </Badge>
-                        <Button className="me-2" onClick={() => {
-                            setPokemonTrainerIndex(index)
-                            setEditModalVisible(true)
-                        }}>Edit</Button>
-                        <Button variant="danger" onClick={() => {
-                            setPokemonTrainerIndex(index)
-                            setDelModalVisible(true)
-                        }}>Delete</Button>
-                    </ListGroup.Item>
-                )
-            }
-        </ListGroup>
+    componentWillMount() {
+        findAllTrainers().then(pokeTrainers => this.setState({pokemonTrainer: pokeTrainers}))
+    }
+
+    findIndex(pk) {
+        return this.state.pokemonTrainer.findIndex((trainer) => trainer.pk === pk)
+    }
+
+    render() {
+        return <div>
+            <h1>Trainers</h1>
+            <Pages itemsInPageLimit={10} items={this.state.pokemonTrainer}
+            mapFn={(pokeTrainer) => <ListGroup.Item as="li"
+                    className="d-flex justify-content-between align-items-start"
+                    key={pokeTrainer.pk}>
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">{pokeTrainer.fields.first_name} {pokeTrainer.fields.last_name}</div>
+                    </div>
+                    <Badge variant="dark" className="btn-primary me-2 align-self-center" pill>
+                        {pokeTrainer.pk}
+                    </Badge>
+                    <Button className="me-2" onClick={() => {
+                        this.setState({
+                            pokemonTrainerIndex: pokeTrainer.pk,
+                            editModalVisible: true
+                        })
+                    }}>Edit</Button>
+                    <Button variant="danger" onClick={() => {
+                        this.setState({
+                            pokemonTrainerIndex: pokeTrainer.pk,
+                            delModalVisible: true
+                        })
+                    }}>Delete</Button>
+                </ListGroup.Item>
+            } />
         <br />
-        <Button onClick={() => setAddModalVisible(true)}>Add</Button>
+        <Button onClick={() => this.setState({addModalVisible: true})}>Add</Button>
         <AddTrainersModal
-            show={addModalVisible}
-            handleClose={() => setAddModalVisible(false)}
-            handleSave={handleSave} />
+            show={this.state.addModalVisible}
+            handleClose={() => this.setState({addModalVisible: false})}
+            handleSave={this.handleSave} />
         <EditTrainersModal
-            show={editModalVisible}
-            handleClose={() => setEditModalVisible(false)}
-            pokemonTrainer={pokemonTrainer}
-            handleEdit={handleEdit}
-            pokemonTrainerIndex={pokemonTrainerIndex} />
+            show={this.state.editModalVisible}
+            handleClose={() => this.setState({editModalVisible: false})}
+            pokemonTrainer={this.state.pokemonTrainer}
+            handleEdit={this.handleEdit}
+            pokemonTrainerIndex={this.findIndex(this.state.pokemonTrainerIndex)} />
         <DelTrainersModal
-            show={delModalVisible}
-            handleClose={() => setDelModalVisible(false)}
-            handleDel={handleDel}
-            primary_key={pokemonTrainer[pokemonTrainerIndex]?.pk} />
-    </div>
+            show={this.state.delModalVisible}
+            handleClose={() => this.setState({delModalVisible: false})}
+            handleDel={this.handleDel}
+            primary_key={this.state.pokemonTrainer[this.findIndex(this.state.pokemonTrainerIndex)]?.pk} />
+        </div>
+    }
 }
