372284a124be09afdb4ab495c8db2dad6d0a5581
Kevin Hui <hui.k@northeastern.edu>
2021-12-12 22:15:34 -0500

    add listings of pokemon types

---
+++ pokedex/src/api/PokemonTypeAPI.js
@@ -0,0 +1,25 @@
+// Network API to interact with a pokemon's types
+import { getResource, postResource } from "./APIUtils";
+var urljoin = require('url-join');
+
+const pokemonInfoTypePrefix = "/pokemon/pokemon_type/";
+
+export const findAllPokemonTypes = () =>
+    getResource(pokemonInfoTypePrefix);
+
+export const getPokemonTypesByNationalNum = (nationalNum) =>
+    getResource(urljoin(pokemonInfoTypePrefix, nationalNum.toString()));
+
+export const getPokemonTypeAssoc = (nationalNum, type) =>
+    getResource(urljoin(pokemonInfoTypePrefix, nationalNum.toString(), type));
+
+export const createPokemonType = (typeAssoc) =>
+    postResource(urljoin(pokemonInfoTypePrefix, "create/"), typeAssoc);
+
+export const updatePokemonType = (nationalNum, type) => {
+    postResource(urljoin(pokemonInfoTypePrefix, "update/", nationalNum.toString(), type, "/"), {});
+}
+
+export const deletePokemonType = (nationalNum, type) => {
+    postResource(urljoin(pokemonInfoTypePrefix, "delete/", nationalNum.toString(), type, "/"));
+}
